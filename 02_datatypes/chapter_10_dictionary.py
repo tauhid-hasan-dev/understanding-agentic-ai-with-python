9a437fb0f2aa68f624fe60382b36e66b609dfdb7
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2025-10-01 15:18:32 +0300

    Add new Python file demonstrating dictionary operations, including creation, modification, and key-value management. Illustrate differences between dictionaries and lists with practical examples.

---
+++ 02_datatypes/chapter_10_dictionary.py
@@ -0,0 +1,39 @@
+# difference bwteen dict and list
+
+muzlu_sut = dict(type="muzlu_sut", taste="sut", sugar=10)
+print("muzlu_sut: ", muzlu_sut)
+
+print(muzlu_sut["type"])
+print(muzlu_sut["taste"])
+print(muzlu_sut["sugar"])
+
+muzlu_sut["sugar"] = 20
+print("muzlu_sut: ", muzlu_sut)
+
+muzlu_sut["salt"] = 1
+print("muzlu_sut: ", muzlu_sut)
+
+muzlu_sut.pop("salt")
+print("muzlu_sut: ", muzlu_sut)
+
+chai_recipe = {}
+chai_recipe["tea"] = 10
+chai_recipe["sugar"] = 10
+chai_recipe["milk"] = 10
+chai_recipe["water"] = 10
+
+print("chai_recipe: ", chai_recipe)
+
+del chai_recipe["water"]
+print("chai_recipe: ", chai_recipe)
+
+print("sugar" in chai_recipe)
+print("salt" in chai_recipe)
+
+print(len(chai_recipe))
+
+print(chai_recipe.keys())
+print(chai_recipe.values())
+print(chai_recipe.items())
+
+
