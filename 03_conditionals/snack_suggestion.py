1f6be4c808ff9f0a5477b741367b8a2dabddc77b
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2025-10-02 18:53:11 +0300

    Add new Python file for snack suggestion logic, prompting user input and providing feedback based on available options. Includes conditional statements for handling user preferences.

---
+++ 03_conditionals/snack_suggestion.py
@@ -0,0 +1,8 @@
+snack = input("Waht is your favorite snack? : ").lower()
+
+
+if snack == "cookies" or snack == "samosa":
+    print(f"Great choise! we will serve you {snack}")
+else:
+    print(f"Sorry {snack} is not avialable, we only serve cookies and samosa")
+
