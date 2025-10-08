f1625c19f8ec1447af09923f8bea7cfc101b0f49
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2025-10-08 18:19:29 +0300

    Add new Python files for chai order demonstration and sales report generation. The `01_duplication.py` file showcases a function to print chai orders, while `02_complex.py` includes functions for fetching, filtering, and summarizing sales data, culminating in a report generation.

---
+++ 05_functions/01_duplication.py
@@ -0,0 +1,7 @@
+def print_order(name, chai_type):
+    print(f"{name} ordered {chai_type} chai")
+
+# To avoid duplicaiton, we are just calling it multiple time get multiple output.
+print_order("Aman", "masala")
+print_order("Tauhid", "Lemon")
+
---
+++ 05_functions/02_complex.py
@@ -0,0 +1,18 @@
+def fetch_sales():
+    print("Fetching the sales data")
+
+def filter_valid_sales():
+    print("Filtering valid sales data")
+
+def summerize_data():
+    print("Summerizing the data")
+
+def generate_report():
+    fetch_sales()
+    filter_valid_sales()
+    summerize_data()
+    print("Report generated")
+
+generate_report()
+
+
