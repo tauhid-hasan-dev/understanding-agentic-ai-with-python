7b2adf91ec132e584eb5c13a5fe614a190cdf52f
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2025-10-09 12:52:04 +0300

    Enhance the demonstration of nonlocal and global keywords by adding print statements to show variable values before and after modification. This improves clarity on variable scope changes within the function.

--- 05_functions/05_nonlocal_global_keywords.py
+++ 05_functions/05_nonlocal_global_keywords.py
@@ -1,9 +1,11 @@
-# Be very careful when you use nonlocal and global
+# Be very careful when you use nonlocal and global 
 
 chai_type = "ginger"
+print("Global variable before changing: ", chai_type)
 
 def understading_non_local_and_global():
     chai_type = "lemon"
+    print(f"Local before convertion' : {chai_type}")
     def testing_nonlocal():
         nonlocal chai_type # non local does not work on any global variable, it only works
         chai_type = "tulsi"
@@ -15,10 +17,10 @@ def understading_non_local_and_global():
             print(f"Converting global from ginger to Black : {chai_type}")
         testing_global()
     testing_nonlocal()
-    print("Outside of the testing global function: ", chai_type)
+    print("Local after convertion: ", chai_type)
 
 understading_non_local_and_global()
 
-print(f"Global : {chai_type}")
+print(f"Global after modification: {chai_type}")
 
 
